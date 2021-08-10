06c609f98687f067a46cc63069bf2f7310eefc43
Nobin <nobinkhan.me@gmail.com>
2021-08-11 02:26:29 +0600

    Fomated files.

--- .pretty_func.sh
+++ .pretty_func.sh
@@ -23,5 +23,26 @@ function color_echo(){
     printf $NO_COLOR
 }
 
+#append @arg to ~/.zshrc
+append_to_zshrc() {
+    local text="$1" zshrc
+    local skip_new_line="${2:-0}"
+
+    if [ -w "$HOME/.zshrc.local" ]; then
+        zshrc="$HOME/.zshrc.local"
+    else
+        zshrc="$HOME/.zshrc"
+    fi;
+
+    if ! grep -Fqs "$text" "$zshrc"; then
+        if [ "$skip_new_line" -eq 1 ]; then
+            printf "%s\\n" "$text" >> "$zshrc"
+        else
+            printf "\\n%s\\n" "$text" >> "$zshrc"
+        fi;
+    fi;
+}
+
 export color_echo
 export pretty_echo
+
